3c727ca9456956b42b33bb8b4b9608eb97c61865
Joe <overtonje@gmail.com>
2020-11-16 10:05:19 -0600

    phase 1

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace guessing_game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Time to guess the secret number!");
+            Console.WriteLine("What is your guess?");
+
+            string guess = Console.ReadLine();
+            Console.WriteLine(guess);
+        }
+    }
+}
